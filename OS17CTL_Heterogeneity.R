b6ccd5e19e9c452406e3967f59366b5cceac3549
Sanjana Rajan <Sanjana.Rajan@nationwidechildrens.org>
2021-07-05 16:46:01 -0400

    uploaded deleted code

---
+++ OS17CTL_Heterogeneity.R
@@ -0,0 +1,137 @@
+source("R:/RESRoberts/Bioinformatics/Analysis/Sanjana/scSeurat.R")
+source("R:/RESRoberts/Bioinformatics/Analysis/Sanjana/Downstream.v2.R")
+#loading libraries
+library(Seurat)
+library(future)
+library(ggplot2)
+# library(fgsea)
+library(msigdbr)
+library(clusterProfiler)
+library(SingleR)
+library(dplyr) # %<%
+library(pheatmap)
+library(RColorBrewer)
+library(viridis) # inferno color palette
+library(grid)
+
+# Create Seurat objects and perform initial QC.  Label original source.
+cx.raw <- tenXLoadQC("R:/RESRoberts/Bioinformatics/scRNAOuts/S0016-zymo/filtered_feature_bc_matrix/", spec = "mixHuman")
+cx.raw <- subset(cx.raw, subset = nFeature_RNA >3500 & nCount_RNA <50000 & percent.mt <15)
+cx.raw$src <- "Culture"
+
+tib.raw <- tenXLoadQC("R:/RESRoberts/Bioinformatics/scRNAOuts/S0018xS0028/filtered_feature_bc_matrix/", spec = "mixHuman")
+tib.raw <- subset(tib.raw, subset = nFeature_RNA >3000 & nCount_RNA <60000 & percent.mt <18)
+tib.raw$src <- "Tibia"
+
+lung.raw <- tenXLoadQC("R:/RESRoberts/Bioinformatics/scRNAOuts/S0024xS0029/filtered_feature_bc_matrix/", spec = "mixHuman")
+lung.raw <- subset(lung.raw, subset = nFeature_RNA >1250 & nCount_RNA <60000 & percent.mt <25)
+lung.raw$src <- "Lung"
+
+# Add lineage tracing tags to the Seurat objects
+cx.raw <- processLTBC(cx.raw,
+                      lt.loc = "R:/RESRoberts/Bioinformatics/scRNAOuts/S0016-zymo/lt.fq",
+                      cid.loc = "R:/RESRoberts/Bioinformatics/scRNAOuts/S0016-zymo/cid.fq",
+                      histogram = T,
+                      title = "Culture, Top 40 Clones",
+                      ymax = 1.75,
+                      col.fill = "#E64B35FF",
+                      relative = T)
+tib.raw <- processLTBC(tib.raw,
+                       lt.loc = "R:/RESRoberts/Bioinformatics/scRNAOuts/S0018xS0028/lt.fq",
+                       cid.loc = "R:/RESRoberts/Bioinformatics/scRNAOuts/S0018xS0028/cid.fq",
+                       histogram = T,
+                       title = "Tibia, Top 40 Clones",
+                       ymax = 1.75,
+                       col.fill = "#00A087FF",
+                       relative = T)
+lung.raw <- processLTBC(lung.raw,
+                        lt.loc = "R:/RESRoberts/Bioinformatics/scRNAOuts/S0024xS0029/lt.fq",
+                        cid.loc = "R:/RESRoberts/Bioinformatics/scRNAOuts/S0024XS0029/cid.fq",
+                        histogram = T,
+                        title = "Lineage Barcode Enrichment by Microenvironment",
+                        ymax = 1.75,
+                        relative = T)
+
+#Subset all to #2800 cells in each condition 
+cx.raw <- subset(cx.raw, cells = sample(Cells(cx.raw), 2800))
+tib.raw <- subset(tib.raw, cells = sample(Cells(tib.raw), 2800))
+lung.raw <- subset(lung.raw, cells = sample(Cells(lung.raw), 2800))
+
+# Merge into a single Seurat object
+os17 <- merge(cx.raw, y = c(tib.raw, lung.raw),
+              add.cell.ids = c("Culture", "Tibia", "Lung"),
+              project = "LineageTracing")
+
+# Process and cluster
+os17 <- NormalizeData(os17) %>%
+  FindVariableFeatures(selection.method = "vst") %>%
+  ScaleData() %>%
+  RunPCA(pc.genes = os.17@var.genes, npcs = 20) %>%
+  RunUMAP(reduction = "pca", dims = 1:20) %>%
+  FindNeighbors(reduction = "pca", dims = 1:20) %>%
+  FindClusters(resolution = 0.3)
+
+# CCR
+# Attempt to regress out the effects of cell cycle on these tumor cells
+s.genes <- cc.genes$s.genes
+g2m.genes <- cc.genes$g2m.genes
+os17 <- CellCycleScoring(object = os17, s.features = s.genes,
+                         g2m.features = g2m.genes, set.ident = TRUE)
+
+os17 <- ScaleData(object = os17, vars.to.regress = c("S.Score", "G2M.Score"),
+                  features = rownames(x = os17)) 
+os17 <- RunPCA(os17, pc.genes = os.17@var.genes, npcs = 20) %>%
+  RunUMAP(reduction = "pca", dims = 1:20) %>%
+  FindNeighbors(reduction = "pca", dims = 1:20) %>%
+  FindClusters(resolution = 0.3)
+
+# os17 <- RunPCA(os17, pc.genes = os17@var.genes, npcs = 20)
+# os17 <- RunHarmony(os17, group.by.vars = "src", plot_convergence = T)
+# os17 <- RunUMAP(os17, reduction = "harmony", dims = 1:20)
+# os17 <- FindNeighbors(os17, reduction = "harmony", dims = 1:20)
+# os17 <- FindClusters(os17, resolution = 0.3)
+
+# pdf("Harmony.pdf", width = 7, height = 7)
+# DimPlot(os17, reduction = "umap", group.by = "src", pt.size = 1, label = F, order = cell.ids) + 
+#   coord_fixed() + 
+#   ggtitle("OS17 by Source") + 
+#   scale_color_npg()
+# dev.off()
+
+# Plot the data 
+set.seed(100)
+cell.ids <- sample(colnames(os17))
+DimPlot(os17, reduction = "umap", group.by = "src", pt.size = 1, label = F, order = cell.ids) + 
+  coord_fixed() + 
+  ggtitle("OS17 by Source") + 
+  scale_color_npg()
+DimPlot(os17, reduction = "umap", pt.size = 1, label = T) + 
+  coord_fixed() + 
+  ggtitle("OS17 Clusters") + 
+  scale_color_npg(alpha = 0.7)
+
+##################Find the optimum resolution for clustering
+os17.nC <- nRes(os17, 
+                res = seq(from = 0.2, to = 0.3, by = 0.01))
+plot <- pSil(os17.nC, 0.3)
+os17.3 <- os17 %>% FindClusters(resolution = 0.3)
+os17.2 <- os17 %>% FindClusters(resolution = 0.2)
+
+# find markers for every cluster compared to all remaining cells, report only the positive ones
+os17.markers <- FindAllMarkers(os17.2, only.pos = FALSE, min.pct = 0.25, logfc.threshold = 0.25)
+
+#Heatmap
+os17.markers %>%
+  group_by(cluster) %>%
+  top_n(n = 5, wt = avg_log2FC) -> top10
+
+# pdf("All.pdf", width = 10, height = 7)
+DoHeatmap(os17.2, features = top10$gene) + NoLegend()
+# dev.off()
+
+#Markers
+VlnPlot(os17.3,
+        features = c("COL1A1", "LUM", "RUNX2", "SOX9", "CTSK", "JUN", "CD74", "SFRP2"),
+        slot = "counts",
+        ncol = 4,
+        log = TRUE)
---
+++ OS17CTL_Heterogeneity_LTpathway.R
@@ -0,0 +1,159 @@
+####################subset to cells that have a lineage tag
+#Culture
+Culture.LTcells <- vector(mode = "character")
+for(i in 1:nrow(cx.lt.list)){
+  temp <- WhichCells(cx.sub, expression = lt == cx.lt.list[[i,1]]) 
+  Culture.LTcells <- append(Culture.LTcells, temp)
+}
+cx.tagged <- subset(cx.sub, cells = Culture.LTcells)
+
+#Tibia
+tib.LTcells <- vector(mode = "character")
+for(i in 1:nrow(tib.lt.list)){
+  temp <- WhichCells(tib.sub, expression = lt == tib.lt.list[[i,1]]) 
+  tib.LTcells <- append(tib.LTcells, temp)
+}
+tib.tagged <- subset(tib.sub, cells = tib.LTcells)
+
+#Lung
+lung.LTcells <- vector(mode = "character")
+for(i in 1:515){
+  temp <- WhichCells(lung.sub, expression = lt == lung.lt.list[[i,1]]) 
+  lung.LTcells <- append(lung.LTcells, temp)
+}
+lung.tagged <- subset(lung.sub, cells = lung.LTcells)
+
+lung.sub <- subset(lung.sub, idents = c("0", "1", "2", "6")) ##Remove outlier cells in cluster 3 and 5
+
+pdf("LungOutline.pdf", height = 5, width = 5)
+DimPlot(lung.sub, reduction = "umap", pt.size = 1, label = TRUE) + 
+  coord_fixed() + 
+  ggtitle("Lung-derived") +
+  xlim(0,9) + NoAxes()+ 
+  scale_color_npg(alpha = 1)
+dev.off()
+
+#Lung
+vplayout <- function(x, y) viewport(layout.pos.row = x, layout.pos.col = y)
+pdf("grid_lung_v4.pdf", width = 10, height = 10)
+grid.newpage()
+pushViewport(viewport(layout = grid.layout(1, 4)))
+for (i in 1:4) {
+  p <- DimPlot(lung.tagged, 
+               reduction = "umap",
+               pt.size = 1,
+               cells.highlight = WhichCells(lung.sub, expression = lt == lung.lt.list[[i,1]]), 
+               cols.highlight = "#4DBBD5FF",
+               sizes.highlight = 3) + 
+    coord_fixed() +
+    theme(legend.position = "none") +
+    ggtitle(paste("Clone", lung.lt.list[[i,1]])) +
+    xlim(0,9) + NoLegend() + NoAxes()
+  print(p, vp=vplayout(ceiling(i/4), i))
+}
+dev.off()
+
+#cell cycle distribution of top 4 lineages - change to Pie chart and percentage
+
+vplayout <- function(x, y) viewport(layout.pos.row = x, layout.pos.col = y)
+pdf("pie.pdf", width = 15, height = 15)
+grid.newpage()
+pushViewport(viewport(layout = grid.layout(1, 4)))
+for(i in 1:4){
+  temp <- WhichCells(lung.sub, expression = lt == lung.lt.list[[i,1]]) 
+  LT.cells <- subset(lung.sub, cells = temp)
+  df <- table(LT.cells$Phase)
+  bp <- ggplot(as.data.frame(df), aes(x="", y=Freq, fill=Var1))+
+    geom_bar(width = 1, stat = "identity")
+  pie <- bp + coord_polar("y", start=0) + scale_fill_brewer(palette="Blues")+
+    theme_minimal() + NoAxes() +
+    ggtitle(paste("Clone", lung.lt.list[[i,1]]))
+  print(pie, vp=vplayout(ceiling(i/4), i))
+}
+dev.off()
+
+#barplot
+bp <-  ggplot(as.data.frame(df), aes(x = Var1, y = Freq)) +
+  geom_bar(fill = "#4DBBD5FF", stat = "identity") +
+  ggtitle("Cell cycle distribution") +
+  ylab("Count") +
+  xlab("Phase")
+
+################Pathways activated in top 10 lineages
+
+lung.lt.list[1:10,1]
+# 052-082154 
+# 039-074146 
+# 037-078792 
+# 039-052640 
+# 095-044696 
+# 039-098388 
+# 064-065939 
+# 013-044371 
+# 063-007191 
+# 018-082025
+
+data <- lung.tagged
+
+#Subset cells of each lineage into a separate cluster in lung tagged
+for(i in 1:10) {
+  cells <- WhichCells(data, expression = lt == lung.lt.list[[i,1]])
+  Idents(object = data, cells = cells) <- paste("Enriched Lineage", i, sep = " ")
+}
+# Idents(data) <- data$seurat_clusters
+# Stash cell identity classes
+data[["Lineage"]] <- Idents(data)
+
+DimPlot(data, split.by = "Lineage")
+#Top markers for each lineage
+
+# find markers for every cluster compared to all remaining cells, report positive and negative ones
+data.markers <- FindAllMarkers(data, only.pos = FALSE, min.pct = 0.25, logfc.threshold = 0.25)
+
+em.hm <- DGEA(data)E
+em.hm.lt <- em.hm[-c(11:14)]
+clust.ids <- clust.ids[-c(11:14)]
+c <- rownames(em.hm.lt)
+#remove "HALLMARK_"
+c <- gsub("HALLMARK_", "", c)
+#remove "_" by removing special characters
+c <- gsub("_", " ", c)
+rownames(em.hm.lt) <- c
+col.breaks=seq(-log10(1),min(max(-log10(em.hm.lt))+1,18),by=0.5)
+col=inferno(length(col.breaks)) # library(viridis)
+col=c("white",colorRampPalette(brewer.pal(n = 7, name ="Reds"))(50))
+pheatmap(-log10(em.hm.lt[,1:length(clust.ids)]),cluster_rows = TRUE,cluster_cols = TRUE,
+         cellwidth = 20,cellheight = 10,treeheight_row = 0,treeheight_col=0,
+         color = col,scale='none',breaks=col.breaks,fontsize = 11)
+
+# DotPlot(data, features = rownames(data)) + RotatedAxis() try with known gene list markers 
+
+
+########################Run IPA on the DEGs from top ten lineages
+#NOTE: After Enriched lineage 5, not more than 25 DEGs with P_adj_val < 0.01
+#Think about if the enrichr data is valid and do we need to run a GSEA for Glycolysis alone
+#How is DEG analysis done, do we filter genes based on pvalue, pr adj p value from FindMarkers output
+#Do we have to re-do the analysis in CCR uncorrected space?
+L1.markers <- FindMarkers(data, ident.1 = "Enriched Lineage 1", ident.2 = NULL, only.pos = FALSE, min.pct = 0.25)
+L2.markers <- FindMarkers(data, ident.1 = "Enriched Lineage 2", ident.2 = NULL, only.pos = FALSE, min.pct = 0.25)
+L3.markers <- FindMarkers(data, ident.1 = "Enriched Lineage 3", ident.2 = NULL, only.pos = FALSE, min.pct = 0.25)
+L4.markers <- FindMarkers(data, ident.1 = "Enriched Lineage 4", ident.2 = NULL, only.pos = FALSE, min.pct = 0.25)
+L5.markers <- FindMarkers(data, ident.1 = "Enriched Lineage 5", ident.2 = NULL, only.pos = FALSE, min.pct = 0.25)
+L6.markers <- FindMarkers(data, ident.1 = "Enriched Lineage 6", ident.2 = NULL, only.pos = FALSE, min.pct = 0.25)
+L7.markers <- FindMarkers(data, ident.1 = "Enriched Lineage 7", ident.2 = NULL, only.pos = FALSE, min.pct = 0.25)
+L8.markers <- FindMarkers(data, ident.1 = "Enriched Lineage 8", ident.2 = NULL, only.pos = FALSE, min.pct = 0.25)
+L9.markers <- FindMarkers(data, ident.1 = "Enriched Lineage 9", ident.2 = NULL, only.pos = FALSE, min.pct = 0.25)
+L10.markers <- FindMarkers(data, ident.1 = "Enriched Lineage 10", ident.2 = NULL, only.pos = FALSE, min.pct = 0.25)
+
+#remove unnecessary columns
+myList <- list(L1.markers, L2.markers, L3.markers, L4.markers, L5.markers, L6.markers, L7.markers, L8.markers, L9.markers, L10.markers)
+myList <- lapply(myList, function(x) { x["p_val"] <- NULL; x[, 2:3] <- NULL; x })
+
+#write markers to excel sheet
+names <- c("L1.markers", "L2.markers", "L3.markers", "L4.markers", "L5.markers", "L6.markers", "L7.markers", "L8.markers", "L9.markers", "L10.markers")
+library(xlsx)
+for (i in 1:10){
+  location <- paste0("R:/RESRoberts/Bioinformatics/Analysis/Sanjana/2020/L",i,".markers.xlsx", sep = "")
+  write.xlsx(myList[[i]], file = location, 
+             col.names = TRUE, row.names = TRUE, append = FALSE)
+} 
